4813fe43f16281c16011faefab1a955f679e0be0
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2023-02-27 21:57:03 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,17 @@
 let yy = 0
+let yyy = 0
 function shiftStars () {
     for (let index2 = 0; index2 <= 3; index2++) {
-        yy = index2 + 1
+        yy = 3 - index2
+        yyy = 4 - index2
         for (let index3 = 0; index3 <= 4; index3++) {
-            led.plotBrightness(index3, yy, led.pointBrightness(index3, index2))
+            led.plotBrightness(index3, yyy, led.pointBrightness(index3, yy))
         }
     }
 }
 function doStars () {
     for (let index = 0; index <= 4; index++) {
-        if (4 < randint(0, 10)) {
+        if (6 < randint(0, 10)) {
             led.plotBrightness(index, 0, randint(50, 190))
         } else {
             led.unplot(index, 0)
